bfe293809c76cba3737cbea7247cd4015f5ee633
Vitalii Rovin <vitaly.04afapib@gmail.com>
2020-05-16 21:09:05 +0700

    add scroll

--- src/main.js
+++ src/main.js
@@ -8,59 +8,78 @@ import "./scripts/skills";
 import "./scripts/works";
 import "./scripts/reviews";
 
-let sliderReviews = () => {
-  const left = document.querySelector(".reviews__btn-left");
-  const right = document.querySelector(".reviews__btn-right");
-  const reviewsList = document.querySelector(".reviews__list");
-  const reviewsItemArr = document.querySelectorAll(".reviews__item");
-  const reviewsBlock = document.querySelector(".reviews__block");
+window.onload = function () {
+  let sliderReviews = () => { //слайдер в поле отзывов
+    const left = document.querySelector(".reviews__btn-left");
+    const right = document.querySelector(".reviews__btn-right");
+    const reviewsList = document.querySelector(".reviews__list");
+    const reviewsItemArr = document.querySelectorAll(".reviews__item");
+    const reviewsBlock = document.querySelector(".reviews__block");
 
-  const computed = getComputedStyle(reviewsBlock);
-  let currentItem = 0;
+    const computed = getComputedStyle(reviewsBlock);
+    let currentItem = 0;
 
-  const calkWidthList = () => { //присвоение ширины блоку list
-    const width = parseInt(computed.width) / 2; //половина ширины блока
+    const calkWidthList = () => { //присвоение ширины блоку list
+      const width = parseInt(computed.width) / 2; //половина ширины блока
 
-    for (let i = 0; i < reviewsItemArr.length - 1; i++) { //присвоение ширины item
-      reviewsItemArr[i].style.width = width + "px";
+      for (let i = 0; i < reviewsItemArr.length - 1; i++) { //присвоение ширины item
+        reviewsItemArr[i].style.width = width + "px";
+      }
+
+      reviewsList.style.width = width * reviewsItemArr.length + "px"; //присвоение ширины list
+    };
+
+    const render = () => { //функция сдвига в стороны на шаг
+      let step = parseInt(computed.width) / 2;
+      const currentRight = step * currentItem;
+
+      reviewsList.style.right = currentRight + "px";
     }
 
-    reviewsList.style.width = width * reviewsItemArr.length + "px"; //присвоение ширины list
+    right.addEventListener("click", function (e) {
+      e.preventDefault();
+      currentItem += 2;
+
+      if (currentItem < reviewsItemArr.length) {
+        render();
+      } else {
+        currentItem = reviewsItemArr.length - 1;
+      }
+    });
+
+    left.addEventListener("click", function (e) {
+      e.preventDefault();
+      currentItem -= 2;
+
+      if (currentItem >= 0 && currentItem < reviewsItemArr.length) {
+        render();
+      } else {
+        currentItem = 0;
+      }
+    });
+
+    document.addEventListener("resize", function () { //вызывать при изменении размера окна
+      calkWidthList();
+      render();
+    })
   };
+  sliderReviews();
 
-  const render = () => { //функция сдвига в стороны на шаг
-    let step = parseInt(computed.width) / 2;
-    const currentRight = step * currentItem;
+  let smoothSlide = () => { //плавный переход якарей
+    const smoothLinks = document.querySelectorAll('a[href^="#"]');
+    for (let smoothLink of smoothLinks) {
+      smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+        const elemToMove = document.querySelector(id);
 
-    reviewsList.style.right = currentRight + "px";
+        elemToMove.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      });
+    }
   }
+  smoothSlide();
+}
 
-  right.addEventListener("click", function (e) {
-    e.preventDefault();
-    currentItem += 2;
-
-    if (currentItem < reviewsItemArr.length) {
-      render();
-    } else {
-      currentItem = reviewsItemArr.length - 1;
-    }
-  });
-
-  left.addEventListener("click", function (e) {
-    e.preventDefault();
-    currentItem -= 2;
-
-    if (currentItem >= 0 && currentItem < reviewsItemArr.length) {
-      render();
-    } else {
-      currentItem = 0;
-    }
-  });
-
-  document.addEventListener("resize", function () { //вызывать при изменении размера окна
-    calkWidthList();
-    render();
-  })
-};
-
-sliderReviews();
